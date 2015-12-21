854eb3201a647ec4bd00b4de3a03882e62dd30a4
fryntiz <fryntiz@gmail.com>
2015-12-21 17:13:28 +0100

    Creada función para cambio automático de imagen en el Slide y además cambio en su descripción (falta cambiar ancla)

--- JS/scriptsgenerales.js
+++ JS/scriptsgenerales.js
@@ -38,11 +38,36 @@ function SlideAutomaticoSmall() {
 	//alert("X tiene valor " + x + "Y tiene valor " + y);
 }
 
-
 //Función para hacer pausa, al estilo "sleep" de otros lenguajes
 function sleep(milliSeconds) {
 	// Obtenemos la fecha en milisegundos desde 1/1/1970
 	var startTime = new Date().getTime();
 	// Hacemos un bucle hasta que la fecha actual sea superior a la fecha inicial mas los milisegundos indicados
 	while (new Date().getTime() < startTime + milliSeconds);
+}
+
+//Sección lateral Derecha para Sitios Webs
+var ArrayImaganesSitiosWebs = new Array("./images/webs/1.jpg", "./images/webs/2.jpg", "./images/webs/3.jpg");
+var LongitudArrayImaganesSitiosWebs = ArrayImaganesSitiosWebs.length;
+var slideWebActual = "0";
+function slideWeb() {
+	var imgWebs = document.getElementById("imgWebs");
+	var descripcionImgWebs = document.getElementById("descripcionImgWebs");
+	var cambioDescripcionImgWebs = new Array(
+//Descripción 1
+		"<H1>Página web 1</H1><BR/><P>Descripción de la página web 1</P>",
+//Descripción 2
+		"<H1>Página web 2</H1><BR/><P>Descripción de la página web 2</P>",
+//Descripción 3
+		"<H1>Página web 3</H1><BR/><P>Descripción de la página web 3</P>"
+	);
+
+
+	if (slideWebActual == LongitudArrayImaganesSitiosWebs) {
+				slideWebActual = 0;
+			}
+	imgWebs.src = ArrayImaganesSitiosWebs[slideWebActual];
+	descripcionImgWebs.innerHTML = cambioDescripcionImgWebs[slideWebActual];
+	++slideWebActual;
+	//alert(cambioDescripcionImgWebs[slideWebActual]);
 }

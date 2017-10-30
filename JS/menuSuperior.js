8f3ee8aee7baafc713e0a9915978f01dd3c2d921
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-30 17:22:24 +0100

    Whitespaces fixed

--- JS/menuSuperior.js
+++ JS/menuSuperior.js
@@ -6,15 +6,15 @@ Menú Superior
 var EstadoMenu = "false";
 //Activar Menú superior
 function activarMenuSuperior () {
-	var menu = document.getElementById("ulMenuMINI");
-	menu.style.display = "block";
-	EstadoMenu = "true";
+    var menu = document.getElementById("ulMenuMINI");
+    menu.style.display = "block";
+    EstadoMenu = "true";
 }
 //Cerrar el Menú superior al pulsar fuera de este
 function cerrarMenuSuperior () {
-	var menu = document.getElementById("ulMenuMINI");
-	if (EstadoMenu == "true") {
-		menu.style.display = "none";
-		EstadoMenu = "false";
-	}
+    var menu = document.getElementById("ulMenuMINI");
+    if (EstadoMenu == "true") {
+        menu.style.display = "none";
+        EstadoMenu = "false";
+    }
 }

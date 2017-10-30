dc075872934b60e62e3f5e5d9d2663f70f03e461
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-30 17:00:53 +0100

    Whitespaces fixed

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,11 +4,11 @@ Actualizar contenido según página actual
 ------------------------------------------------
 */
 function rotar(seleccion, cargarBiblioteca) {
-	$(document).ready(function() {
-				$(seleccion).click(function(){
-					$("#contenidoweb").load(cargarBiblioteca);
-				});
-		});
+    $(document).ready(function() {
+                $(seleccion).click(function(){
+                    $("#contenidoweb").load(cargarBiblioteca);
+                });
+        });
 }
 
 
@@ -18,20 +18,20 @@ Cambiar id de "BODY" al pulsar menú principal
 ------------------------------------------------
 */
 function cambiarIdBody(x) {
-	var selectorBody = document.body;
+    var selectorBody = document.body;
 
-	if (x == "paginicio") {
-		selectorBody.id = "paginicio";
-	} else if (x == "pagcv") {
-		selectorBody.id = "pagcv";
-	} else if (x == "pagcvext") {
-		selectorBody.id = "pagcvext";
-	} else if (x == "pagdescargas") {
-		selectorBody.id = "pagdescargas";
-	} else if (x == "pagcontacto") {
-		selectorBody.id = "pagcontacto";
-	} else if (x == "pagmasinfo") {
-		selectorBody.id = "pagmasinfo";
-	}
-	cerrarMenuSuperior();//Ejecuta función para cerrar el menú
+    if (x == "paginicio") {
+        selectorBody.id = "paginicio";
+    } else if (x == "pagcv") {
+        selectorBody.id = "pagcv";
+    } else if (x == "pagcvext") {
+        selectorBody.id = "pagcvext";
+    } else if (x == "pagdescargas") {
+        selectorBody.id = "pagdescargas";
+    } else if (x == "pagcontacto") {
+        selectorBody.id = "pagcontacto";
+    } else if (x == "pagmasinfo") {
+        selectorBody.id = "pagmasinfo";
+    }
+    cerrarMenuSuperior();//Ejecuta función para cerrar el menú
 }

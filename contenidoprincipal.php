4c69d313f2a049b93d946b0b10cc6cb7dfd68ef0
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-30 16:54:16 +0100

    Whitespaces fix

--- contenidoprincipal.php
+++ contenidoprincipal.php
@@ -1,95 +1,95 @@
-			<DIV id="anillas"></DIV>
-			<DIV id="contenidoAnillasSuperior" class="anillasArriba"></DIV>
-			<SECTION>
-				<HEADER class="cajaTitulo">
-					<H2>INICIO</H2>
-					<P>
-						Este curriculum online prentende mostrar los conocimientos y aptitudes de los cuales dispongo, así como la experiencia profesional.
-						<BR/><BR/>
-						En esta página principal se encuentra un resumen de lo que se encontrará en el resto de secciones.
-					</P>
-				</HEADER>
+            <DIV id="anillas"></DIV>
+            <DIV id="contenidoAnillasSuperior" class="anillasArriba"></DIV>
+            <SECTION>
+                <HEADER class="cajaTitulo">
+                    <H2>INICIO</H2>
+                    <P>
+                        Este curriculum online prentende mostrar los conocimientos y aptitudes de los cuales dispongo, así como la experiencia profesional.
+                        <BR/><BR/>
+                        En esta página principal se encuentra un resumen de lo que se encontrará en el resto de secciones.
+                    </P>
+                </HEADER>
 
-				<ARTICLE>
-					<HEADER class="cajaTituloDeArticulo">
-						<H2 class="tituloDeArticulo">PÁGINA PRINCIPAL, "PRESENTACIÓN"</H2>
-						<P class="subtituloDeArticulo"></P>
-					</HEADER>
-					<HEADER>
-						<H3>¿Por que esta página web?</H3>
-					</HEADER>
+                <ARTICLE>
+                    <HEADER class="cajaTituloDeArticulo">
+                        <H2 class="tituloDeArticulo">PÁGINA PRINCIPAL, "PRESENTACIÓN"</H2>
+                        <P class="subtituloDeArticulo"></P>
+                    </HEADER>
+                    <HEADER>
+                        <H3>¿Por que esta página web?</H3>
+                    </HEADER>
 
-					<P>
-						Esta página web está creada para mostrar mi <STRONG>curriculum</STRONG>, aptitudes y conocimientos en el campo de la informática en general y la programación de aplicaciones WEB en especial.
-					</P>
-				</ARTICLE>
+                    <P>
+                        Esta página web está creada para mostrar mi <STRONG>curriculum</STRONG>, aptitudes y conocimientos en el campo de la informática en general y la programación de aplicaciones WEB en especial.
+                    </P>
+                </ARTICLE>
 
-				<ARTICLE>
-					<HEADER>
-						<H3>¿Quien soy?</H3>
-					</HEADER>
+                <ARTICLE>
+                    <HEADER>
+                        <H3>¿Quien soy?</H3>
+                    </HEADER>
 
-					<P>
-						Mi nombre es <STRONG>Raúl Caro Pastorino</STRONG>, firmo con el sobrenombre: "<STRONG>fryntiz</STRONG>".
-					</P>
-					<P>
-						Desde siempre me ha gustado y apasionado la informática, las nuevas tecnologías, la programación y microelectrónica.
-					</P>
-					<P>
-						En los últimos años he centrado mi interés y estudios hacia la programación de aplicaciones WEB en lenguajes como HTML, CSS, JS, PHP y además MYSQL.
-					</P>
-					<P>
-						Anteriormente he alternado entre la reparación de computadoras y otros aparatos con la programación de sitios WEBS y/o CMS como Drupal o Wordpress, tanto como su administración y la creación de contenido para estas.
-					</P>
-					<P>
-						A pesar de disponer de poca experiencia profesional acreditada si que dispongo de bastante experiencia en reparación de equipos y creación de sitios WEBS HTML+CSS+JS para pequeñas empresas o individuales como:
-						<UL>
-							<LI>Plantillas</LI>
-							<LI>Oportunidades</LI>
-							<LI>Galerías de fotos</LI>
-							<LI>Este mismo curriculum</LI>
-							<LI>Gestión de servidores PHP+Python</LI>
-							<LI>Gestión de servidores PHP+ShellScript</LI>
-						</UL>
-					</P>
-				</ARTICLE>
+                    <P>
+                        Mi nombre es <STRONG>Raúl Caro Pastorino</STRONG>, firmo con el sobrenombre: "<STRONG>fryntiz</STRONG>".
+                    </P>
+                    <P>
+                        Desde siempre me ha gustado y apasionado la informática, las nuevas tecnologías, la programación y microelectrónica.
+                    </P>
+                    <P>
+                        En los últimos años he centrado mi interés y estudios hacia la programación de aplicaciones WEB en lenguajes como HTML, CSS, JS, PHP y además MYSQL.
+                    </P>
+                    <P>
+                        Anteriormente he alternado entre la reparación de computadoras y otros aparatos con la programación de sitios WEBS y/o CMS como Drupal o Wordpress, tanto como su administración y la creación de contenido para estas.
+                    </P>
+                    <P>
+                        A pesar de disponer de poca experiencia profesional acreditada si que dispongo de bastante experiencia en reparación de equipos y creación de sitios WEBS HTML+CSS+JS para pequeñas empresas o individuales como:
+                        <UL>
+                            <LI>Plantillas</LI>
+                            <LI>Oportunidades</LI>
+                            <LI>Galerías de fotos</LI>
+                            <LI>Este mismo curriculum</LI>
+                            <LI>Gestión de servidores PHP+Python</LI>
+                            <LI>Gestión de servidores PHP+ShellScript</LI>
+                        </UL>
+                    </P>
+                </ARTICLE>
 
-				<ARTICLE>
-					<HEADER>
-						<H3>Mis aptitudes más generales</H3>
-					</HEADER>
+                <ARTICLE>
+                    <HEADER>
+                        <H3>Mis aptitudes más generales</H3>
+                    </HEADER>
 
-					<P>
-						<UL>
-							<LI>Sistemas Operativos variados como:
-								<UL>
-									<LI>Windows</LI>
-									<LI>Linux/GNU (En especial este S.O)</LI>
-									<LI>Apple MacOSX</LI>
-									<LI>Android</LI>
-								</UL>
-							</LI>
-							<LI>Buena Organización</LI>
-							<LI>Aprendo muy rápido</LI>
-							<LI>Conozco gran variadad de software multiplataforma</LI>
-							<LI>Alta disponibilidad para un trabajo</LI>
-							<LI>Trabajar lo hago con el mismo agrado que si fuese un hobbie</LI>
-						</UL>
-					</P>
-				</ARTICLE>
+                    <P>
+                        <UL>
+                            <LI>Sistemas Operativos variados como:
+                                <UL>
+                                    <LI>Windows</LI>
+                                    <LI>Linux/GNU (En especial este S.O)</LI>
+                                    <LI>Apple MacOSX</LI>
+                                    <LI>Android</LI>
+                                </UL>
+                            </LI>
+                            <LI>Buena Organización</LI>
+                            <LI>Aprendo muy rápido</LI>
+                            <LI>Conozco gran variadad de software multiplataforma</LI>
+                            <LI>Alta disponibilidad para un trabajo</LI>
+                            <LI>Trabajar lo hago con el mismo agrado que si fuese un hobbie</LI>
+                        </UL>
+                    </P>
+                </ARTICLE>
 
-				<ARTICLE>
-					<HEADER>
-						<H3>Aptitudes que planeo adquirir en un futuro cercano</H3>
-					</HEADER>
+                <ARTICLE>
+                    <HEADER>
+                        <H3>Aptitudes que planeo adquirir en un futuro cercano</H3>
+                    </HEADER>
 
-					<P>
-						<UL>
-							<LI>Bases de datos PostgreSQL</LI>
-							<LI>Bases de datos MongoDB</LI>
-							<LI>Avanzar con python para la WEB (django)</LI>
-							<LI>Ampliar la base de Ajax</LI>
-						</UL>
-					</P>
-				</ARTICLE>
-			</SECTION>
+                    <P>
+                        <UL>
+                            <LI>Bases de datos PostgreSQL</LI>
+                            <LI>Bases de datos MongoDB</LI>
+                            <LI>Avanzar con python para la WEB (django)</LI>
+                            <LI>Ampliar la base de Ajax</LI>
+                        </UL>
+                    </P>
+                </ARTICLE>
+            </SECTION>

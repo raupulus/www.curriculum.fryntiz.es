e4e790474e45f4ba1155f17b4849d9b0c06b3c25
fryntiz <fryntiz@gmail.com>
2017-10-30 13:10:17 +0100

    Cambio en el correo

--- barraderecha.php
+++ barraderecha.php
@@ -18,7 +18,7 @@
 						<A href="https://plus.google.com/109655402661721084499" title="fryntiz Google Plus" target="_blank"><DIV id="googleplus"></DIV></A>
 						<A href="https://www.facebook.com/fryntiz" title="fryntiz Facebook" target="_blank"><DIV id="facebook"></DIV></A>
 						<A href="https://twitter.com/fryntiz" title="fryntiz Twitter" target="_blank"><DIV id="twitter"></DIV></A>
-						<A href="mailto:fryntiz@gmail.com" title="fryntiz Gmail"><DIV id="gmail"></DIV></A>
+						<A href="mailto:tecnico@fryntiz.es" title="fryntiz Correo Electrónico"><DIV id="gmail"></DIV></A>
 						<A href="https://www.youtube.com/channel/UC6CHhi3UTZ6CJPJUta-IcXw" title="fryntiz Youtube" target="_blank"><DIV id="youtube"></DIV></A>
 						<A href="https://drive.google.com/folderview?id=0B6aZ5irkbzxuRkNiNU9ZdGVuemc&usp=sharing" title="fryntiz Google Drive" target="_blank"><DIV id="googleDrive"></DIV></A>
 						<A href="https://www.dropbox.com/sh/cyzs1mtsy1qq7oz/AACsl1A65mTyBeIQG3XMZhcYa?dl=0" title="fryntiz Dropbox" target="_blank"><DIV id="dropbox"></DIV></A>

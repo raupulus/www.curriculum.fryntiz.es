1abf14913d25b555c6925bca4dfdc07af29cd43d
fryntiz <fryntiz@gmail.com>
2017-12-27 17:22:47 +0100

    Etiquetas en minúsculas y reestructuradas

--- barraderecha.php
+++ barraderecha.php
@@ -1,29 +1,65 @@
-            <ASIDE id="menuderecha"><!--Sección lateral Derecha-->
-                <DIV class="anillasArriba"></DIV>
-                <DIV>
-                    <DIV id="mostrarwebs" onMouseOver="MouseIsHover('true')" onMouseOut="MouseIsHover('false')">
-                        <A id="sourcesSitiosWeb" href="#" target="_blank" title="Mis sitios WEB Fryntiz">
-                            <IMG id="imgWebs" src="images/webs/1.jpg" alt="Descripción del sitio WEB"/>
+<aside id="menuderecha"><!--Sección lateral Derecha-->
+    <div class="anillasArriba"></div>
+    <div>
+        <div id="mostrarwebs" onMouseOver="MouseIsHover('true')"
+             onMouseOut="MouseIsHover('false')">
+            <a id="sourcesSitiosWeb" href="#" target="_blank"
+               title="Mis sitios WEB Fryntiz">
+                <img id="imgWebs" src="images/webs/1.jpg" alt="Descripción del sitio WEB" />
 
-                            <P id="descripcionImgWebs"><!--Caja con descripción de la imagen actual-->
+                <!--Caja con descripción de la imagen actual-->
+                <p id="descripcionImgWebs"></p>
+            </a>
+        </div>
+    </div>
 
-                            </P>
-                        </A>
-                    </DIV>
-                </DIV>
+    <div id="cajaredes">
+        <div class="anillasArriba"></div>
+        <div id="redes">
+            <a href="https://plus.google.com/109655402661721084499"
+               title="fryntiz Google Plus" target="_blank">
+                <div id="googleplus"></div>
+            </a>
 
-                <DIV id="cajaredes">
-                    <DIV class="anillasArriba"></DIV>
-                    <DIV id="redes">
-                        <A href="https://plus.google.com/109655402661721084499" title="fryntiz Google Plus" target="_blank"><DIV id="googleplus"></DIV></A>
-                        <A href="https://www.facebook.com/fryntiz" title="fryntiz Facebook" target="_blank"><DIV id="facebook"></DIV></A>
-                        <A href="https://twitter.com/fryntiz" title="fryntiz Twitter" target="_blank"><DIV id="twitter"></DIV></A>
-                        <A href="mailto:tecnico@fryntiz.es" title="fryntiz Correo Electrónico"><DIV id="gmail"></DIV></A>
-                        <A href="https://www.youtube.com/channel/UC6CHhi3UTZ6CJPJUta-IcXw" title="fryntiz Youtube" target="_blank"><DIV id="youtube"></DIV></A>
-                        <A href="https://drive.google.com/folderview?id=0B6aZ5irkbzxuRkNiNU9ZdGVuemc&usp=sharing" title="fryntiz Google Drive" target="_blank"><DIV id="googleDrive"></DIV></A>
-                        <A href="https://www.dropbox.com/sh/cyzs1mtsy1qq7oz/AACsl1A65mTyBeIQG3XMZhcYa?dl=0" title="fryntiz Dropbox" target="_blank"><DIV id="dropbox"></DIV></A>
-                        <A href="https://github.com/fryntiz" title="fryntiz GitHub" target="_blank"><DIV id="github"></DIV></A>
-                        <A href="http://www.linkedin.com/in/fryntiz" title="fryntiz Linkedin" target="_blank"><DIV id="linkedin"></DIV></A>
-                    </DIV>
-                </DIV>
-            </ASIDE>
+            <a href="https://www.facebook.com/fryntiz" title="fryntiz Facebook"
+               target="_blank">
+                <div id="facebook"></div>
+            </a>
+
+            <a href="https://twitter.com/fryntiz" title="fryntiz Twitter"
+               target="_blank">
+                <div id="twitter"></div>
+            </a>
+
+            <a href="mailto:tecnico@fryntiz.es"
+               title="fryntiz Correo Electrónico">
+                <div id="gmail"></div>
+            </a>
+
+            <a href="https://www.youtube.com/channel/UC6CHhi3UTZ6CJPJUta-IcXw"
+               title="fryntiz Youtube" target="_blank">
+                <div id="youtube"></div>
+            </a>
+
+            <a href="https://drive.google.com/folderview?id=0B6aZ5irkbzxuRkNiNU9ZdGVuemc&usp=sharing"
+            title="fryntiz Google Drive" target="_blank">
+                <div id="googleDrive"></div>
+            </a>
+
+            <a href="https://www.dropbox.com/sh/cyzs1mtsy1qq7oz/AACsl1A65mTyBeIQG3XMZhcYa?dl=0"
+            title="fryntiz Dropbox" target="_blank">
+                <div id="dropbox"></div>
+            </a>
+
+            <a href="https://github.com/fryntiz" title="fryntiz GitHub"
+               target="_blank">
+                <div id="github"></div>
+            </a>
+
+            <a href="http://www.linkedin.com/in/fryntiz"
+               title="fryntiz Linkedin" target="_blank">
+                <div id="linkedin"></div>
+            </a>
+        </div>
+    </div>
+</aside>

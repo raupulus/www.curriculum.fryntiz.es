3cfa6d7f91db3126eebf6fe5e710ba33719b1871
fryntiz <fryntiz@gmail.com>
2017-12-27 17:24:11 +0100

    Agregada cabecera con autoría

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
+<?php
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2017 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
+?>
+
 <HTML lang="es">
     <HEAD>
         <?php

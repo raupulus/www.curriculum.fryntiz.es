d3d502aa9caf1d7965411a8316e66e8f63996e6e
fryntiz <fryntiz@gmail.com>
2017-12-27 16:38:08 +0100

    Replanteada sintaxis

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,37 +1,36 @@
-/*
-------------------------------------------------
-Actualizar contenido según página actual
-------------------------------------------------
-*/
+
+/**
+ * Actualizar contenido según página actual
+ * @param  {[type]} seleccion        [description]
+ * @param  {[type]} cargarBiblioteca [description]
+ */
 function rotar(seleccion, cargarBiblioteca) {
     $(document).ready(function() {
                 $(seleccion).click(function(){
-                    $("#contenidoweb").load(cargarBiblioteca);
+                    $('#contenidoweb').load(cargarBiblioteca);
                 });
         });
 }
 
-
-/*
-------------------------------------------------
-Cambiar id de "BODY" al pulsar menú principal
-------------------------------------------------
-*/
+/**
+ * Cambiar id de "BODY" al pulsar menú principal
+ * @param  {[type]} x [description]
+ */
 function cambiarIdBody(x) {
     var selectorBody = document.body;
 
-    if (x == "paginicio") {
-        selectorBody.id = "paginicio";
-    } else if (x == "pagcv") {
-        selectorBody.id = "pagcv";
-    } else if (x == "pagcvext") {
-        selectorBody.id = "pagcvext";
-    } else if (x == "pagdescargas") {
-        selectorBody.id = "pagdescargas";
-    } else if (x == "pagcontacto") {
-        selectorBody.id = "pagcontacto";
-    } else if (x == "pagmasinfo") {
-        selectorBody.id = "pagmasinfo";
+    if (x == 'paginicio') {
+        selectorBody.id = 'paginicio';
+    } else if (x == 'pagcv') {
+        selectorBody.id = 'pagcv';
+    } else if (x == 'pagcvext') {
+        selectorBody.id = 'pagcvext';
+    } else if (x == 'pagdescargas') {
+        selectorBody.id = 'pagdescargas';
+    } else if (x == 'pagcontacto') {
+        selectorBody.id = 'pagcontacto';
+    } else if (x == 'pagmasinfo') {
+        selectorBody.id = 'pagmasinfo';
     }
-    cerrarMenuSuperior();//Ejecuta función para cerrar el menú
+    cerrarMenuSuperior(); // Ejecuta función para cerrar el menú superior
 }

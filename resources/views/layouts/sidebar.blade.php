72c9d98d7c73676fd5597a50f3b61790a83d8b9e
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-26 20:58:50 +0200

    Creo layout para sidebar

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,16 @@
+<!--
+@author Raúl Caro Pastorino
+@copyright Copyright (c) 2018 Raúl Caro Pastorino
+@license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+
+Author Web: https://fryntiz.es
+E-mail: dev@fryntiz.es
+-->
+<aside>
+    Este es el sidebar
+    <ul>
+        <li>Elemento 1</li>
+        <li>Elemento 2</li>
+        <li>Elemento 3</li>
+    </ul>
+</aside>

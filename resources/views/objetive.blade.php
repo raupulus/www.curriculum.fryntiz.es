bb6b04fea1ad4c6c95292a63b8b0c39a6f89a67f
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-26 20:59:42 +0200

    Extiendo cada sección de la plantilla

--- resources/views/objetive.blade.php
+++ resources/views/objetive.blade.php
@@ -1,23 +1,24 @@
-<!DOCTYPE html>
-<!----------------------------------------------------------
+<!--
 @author Raúl Caro Pastorino
 @copyright Copyright (c) 2018 Raúl Caro Pastorino
 @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
 
 Author Web: https://fryntiz.es
 E-mail: dev@fryntiz.es
----------------------------------------------------------->
-<html lang="es">
-  <head>
-    <meta charset="utf-8">
-    <meta name="description" content="Education" />
-    <meta name="keywords" content="Raúl Caro Pastorino, Fryntiz" />
-    <meta name="author" content="Raúl Caro Pastorino" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Objetive</title>
-  </head>
+-->
 
-  <body>
-    <h1>Objetive</h1>
-  </body>
-</html>
+@extends('layouts.template')
+
+{{-- Título de la página --}}
+@section('title', 'Objetive')
+
+{{-- Descripción --}}
+@section('description', 'Objetive')
+
+{{-- Palabras clave --}}
+@section('keywords', 'curriculum, curriculum online, Raúl Caro Pastorino')
+
+{{-- Contenido de la sección --}}
+@section('content')
+    <h2>contenido</h2>
+@stop

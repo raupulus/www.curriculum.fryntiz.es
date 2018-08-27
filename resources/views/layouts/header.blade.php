b3cd82b10152a4217acac589172660c9edb8a0ba
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-27 11:44:32 +0200

    Comienzo planteamiento de Header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,15 +6,40 @@
 Author Web: https://fryntiz.es
 E-mail: dev@fryntiz.es
 -->
+
+<style>
+    .active {
+        font-weight: bold;
+        color: red;
+        background-color: yellow;
+    }
+
+    .inactive {
+        font-weight: bold;
+        color: green;
+    }
+</style>
+
+<?php
+    function activeMenu($url) {
+        return request()->is($url) ? 'active' : 'inactive';
+    }
+?>
+
 <header>
     <h1>@yield('title')</h1>
     <nav>
         <ul>
-            <li><a href="{{ route('home') }}">Home</a></li>
-            <li><a href="{{ route('objetive') }}">Objetive</a></li>
-            <li><a href="{{ route('experience') }}">Experience</a></li>
-            <li><a href="{{ route('education') }}">Education</a></li>
-            <li><a href="{{ route('skills') }}">Skills</a></li>
+            <li><a class="{{ activeMenu('/') }}"
+                   href="{{ route('home') }}">Home</a></li>
+            <li><a class="{{ activeMenu('objetive') }}"
+                   href="{{ route('objetive') }}">Objetive</a></li>
+            <li><a class="{{ activeMenu('experience') }}"
+                   href="{{ route('experience') }}">Experience</a></li>
+            <li><a class="{{ activeMenu('education') }}"
+                   href="{{ route('education') }}">Education</a></li>
+            <li><a class="{{ activeMenu('skills') }}"
+                   href="{{ route('skills') }}">Skills</a></li>
         </ul>
     </nav>
 </header>

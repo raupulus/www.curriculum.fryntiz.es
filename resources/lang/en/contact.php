7c940a3f246adc74d51791ef3d661804260ae1ea
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-30 14:18:29 +0200

    Planteo traducciones para la vista de contacto

---
+++ resources/lang/en/contact.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title'                 => 'Form Contact',
+    'first_name'            => 'Name',
+    'last_name'             => 'Last Name',
+    'email'                 => 'E-mail',
+    'subject'               => 'Subject',
+    'message'               => 'Message',
+    'description'           => 'Description',
+    'registered'            => 'Registered at day',
+    'submit'                => 'Send Email',
+];
---
+++ resources/lang/es/contact.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title'                 => 'Formulario de contacto',
+    'first_name'            => 'Nombre',
+    'last_name'             => 'Apellido',
+    'email'                 => 'Correo electrónico',
+    'subject'               => 'Asunto',
+    'message'               => 'Mensaje',
+    'description'           => 'Descripción',
+    'registered'            => 'Registrado el día',
+    'submit'                => 'Enviar Email',
+];

88edc3e1cf41f6519d69764fca5b74ead22642d0
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-09-05 10:36:44 +0200

    Elimino función obsoleta para enviar email

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -36,26 +36,4 @@ class PagesController extends Controller
   {
     return view('contact');
   }
-
-  /*
-   * Valida y comprueba el formulario de contacto para posteriormente enviar
-   * el email.
-   */
-  public function sendMail(\App\Http\Requests\CreateMessageRequest $request)
-  {
-    // Devuelvo temporalmente los datos del formulario como JSON
-    $msg = 'Procesando formulario con los siguientes datos:';
-
-    foreach($request->all() as $name => $val) {
-        //$msg .= '<br />'.$name.': '.$val.'<br />';
-        $msg .= $name.': '.$val;
-    }
-
-    //Session::flash('sucess_message', $msg);
-    return redirect()->route('contact')
-                     ->with('sucess_message', $msg)
-                     ->withCookie('sendmail', true, 2)  // Indica que envió un email
-                     ->withCookie('sendmail_at', time(), 2);
-    //return $request->all();
-  }
 }
